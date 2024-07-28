90f275b63d6654974e806f350e61518f6d7a0879
Sanjeev <sanjeevsagar013>
2024-07-28 23:36:25 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code  as newer version
+
+// alert(3+5) --->we are  using nodejs ,not browser
+
+
+//number---> 2 pow 53
+//bigint --> 
+//string==>""
+//boolean --> true/false
+//null -->  standalone value
+//undefined--> 
+//symbol -->unique
+
+
+//object
+
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null)//object
+
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId= 1233              // not change
+let accounEmail= "sanjeev"
+var accountPaasword="12345"
+accounCity="Delhi"
+
+console.log(accountId)
+
+
+console.table([accountId,accounEmail,accountPaasword,accounCity])
+
+//Prefer not to use var 
+// because  of issue in block scope and functional scope
