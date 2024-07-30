5d6976e97fa037ddfb57fee3a5456ab0042268ff
Sanjeev <sanjeevsagar013>
2024-07-30 15:12:50 +0530

    changein location of files

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,52 @@
+// let score =true
+
+// console.log(typeof score)
+
+
+// let value = Number(score)
+
+// console.log(typeof score)
+
+// //"33"
+// //"33qbc" --> NAN
+
+// //true-->1 false-->0
+
+// //"" --> false
+// //"sanjeev" --> true
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+
+// let someNumber =33
+// ****************************************OPERATIONS*************************************
+
+let value =3
+let negValue= -value
+
+// console.log(2+3);
+// console.log(2/3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2-3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basics/datatype_Summary.js
@@ -0,0 +1,12 @@
+/* types of datatypes
+ Primitive //call by value
+String ,Number,Boolean,null,undefined,Symbol,Bigint
+
+
+
+RefernceType--> Non Primitive
+Array,object,Functions
+
+
+*/ 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
