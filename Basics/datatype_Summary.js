4d9c734c884766bb944c77243d7eb8d8b4af84e8
Sanjeev <sanjeevsagar013>
2024-07-30 15:48:24 +0530

    String

--- Basics/datatype_Summary.js
+++ Basics/datatype_Summary.js
@@ -10,3 +10,28 @@ Array,object,Functions
 
 */ 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+// ******************************************************************************************
+/*
+memory use in JS
+
+Stack(Primitive)
+Heap(Non Primitive)
+
+
+
+
+
+
+
+*/
+
+let user ={
+    email:"user@gmail.com",
+    upi:"sanjeev@ybl"
+}
+
+let userTwo =user
+userTwo.email="sanjeev2020@google.com"
+
+console.log(user.email);
+console.log(userTwo.email)
---
+++ Basics/string.js
@@ -0,0 +1,7 @@
+const name = "sanjeev"
+const repoCount =14
+
+console.log(name+repoCount + "value")
+console.log('user1')
+console.log(typeof String)
+
