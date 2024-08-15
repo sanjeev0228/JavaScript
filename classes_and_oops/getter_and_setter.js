0f98b4754aaa13a88d22a5382ded634c4e698ded
Sanjeev <sanjeevsagar013>
2024-08-15 22:01:10 +0530

    closure

---
+++ classes_and_oops/getter_and_setter.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const sanjeev = new User("sanjeev@ai.com", "abc")
+console.log(sanjeev.email);
