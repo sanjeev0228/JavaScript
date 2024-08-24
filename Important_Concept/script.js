88323f041d6db33a2e6db226f49428cd854a06a9
Sanjeev <sanjeevsagar013>
2024-08-24 08:54:06 +0530

    fetch

---
+++ Important_Concept/script.js
@@ -0,0 +1,22 @@
+// console.log("hello");
+
+//  ()=>{
+//     console.log("JAVAscript");
+// }  
+
+
+// Spread and Rest Operator
+
+
+/* Promises */
+//  new Promise ((resolve,reject)=>{
+
+//     setTimeout(()=> {
+//         resolve("work do e")
+
+//     })
+
+//  })
+
+
+//  async and wait
